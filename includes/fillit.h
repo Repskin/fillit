42733d381a4aaaef5e4f3ce82ac3feb57a47664e
Repskin <repskin.eu@gmail.com>
2017-10-05 16:38:35 -0700

    Update fillit.h

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,8 +6,13 @@
 /*   By: tburnouf <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/05 16:09:53 by tburnouf          #+#    #+#             */
-/*   Updated: 2017/10/05 16:09:56 by tburnouf         ###   ########.fr       */
+/*   Updated: 2017/10/05 16:36:14 by tburnouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FILLIT_H
+# define FILLIT_H
 
+# include <unistd.h>
+
+#endif
